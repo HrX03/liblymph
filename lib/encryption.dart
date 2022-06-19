73da8bcec8c4ad3d9161a415031ae686ffd0fc5d
HrX03 <dn.bianco03@gmail.com>
2022-06-19 20:01:50 +0200

    liblymph: Move blake implementation to lib

--- lib/encryption.dart
+++ lib/encryption.dart
@@ -1,5 +1,6 @@
 library encryption;
 
 export 'package:liblymph/src/encryption/base.dart';
+export 'package:liblymph/src/encryption/blake/stub.dart';
 export 'package:liblymph/src/encryption/boringssl.dart';
 export 'package:liblymph/src/encryption/dart.dart';
