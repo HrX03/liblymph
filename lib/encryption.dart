322d605500c4bb7a2f47107b7669b8c3094490d6
HrX03 <dn.bianco03@gmail.com>
2022-06-19 19:55:38 +0200

    liblymph: Add encryption lib

---
+++ lib/encryption.dart
@@ -0,0 +1,5 @@
+library encryption;
+
+export 'package:liblymph/src/encryption/base.dart';
+export 'package:liblymph/src/encryption/boringssl.dart';
+export 'package:liblymph/src/encryption/dart.dart';
--- lib/providers.dart
+++ lib/providers.dart
@@ -1,3 +1,5 @@
+library providers;
+
 export 'package:liblymph/src/providers/directories.dart';
 export 'package:liblymph/src/providers/preferences.dart';
 export 'package:liblymph/src/providers/providers.dart';
