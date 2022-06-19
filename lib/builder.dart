820a2f9389b7eb0a59c5e666bb63db2f4c765f0b
HrX03 <dn.bianco03@gmail.com>
2022-06-19 17:11:57 +0200

    liblymph: Move generator to its own package

--- lib/builder.dart
+++
@@ -1,4 +0,0 @@
-import 'package:build/build.dart';
-import 'package:liblymph/src/providers/preferences_gen.dart';
-
-Builder preferencesBuilder(BuilderOptions options) => PreferencesBuilder();
--- lib/providers.dart
+++ lib/providers.dart
@@ -1,4 +1,3 @@
 export 'package:liblymph/src/providers/directories.dart';
 export 'package:liblymph/src/providers/preferences.dart';
-export 'package:liblymph/src/providers/preferences_gen.dart';
 export 'package:liblymph/src/providers/providers.dart';
